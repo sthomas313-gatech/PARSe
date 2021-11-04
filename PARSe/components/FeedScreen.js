8e1491cf10444c418195a55cd2d02edd11c641f4
Patrick Crawford <pac410@gmail.com>
2021-11-04 11:19:49 -0400

    feed screen update

--- PARSe/components/FeedScreen.js
+++ PARSe/components/FeedScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   StyleSheet,
   ScrollView,
@@ -14,22 +14,38 @@ import NavBar from './NavBar';
 // import static content (replace with dynamic content from backend later)
 import { recs } from "../static_content";
 
+// firestore
+import firestore from '@react-native-firebase/firestore';
+
+async function get_recs() {
+  const recs_db = firestore().collection("recs");
+  const rec = await recs_db.doc("recID1").get();
+  return rec;
+}
+
+
 
 export default function FeedScreen( {navigation} ) {
+  
+  const [db_recs, setDb_recs] = useState("");
 
-  // build array of RecCard components
+  /* create list of RecCard elements */
   var recCardsList = [];
   for (var i=0; i < recs.length; i++) {
     recCardsList.push(<RecCard key={recs[i].restaurant.name.concat(recs[i].user.username)} rec={recs[i]} />);
   }
+
+  /* Get Firestore Data */
+  // const rec = get_recs();
+  // setDb_recs(JSON.stringify(rec));
 
   return (
     <SafeAreaView style={styles.container}>
         <Header />
         <ScrollView>
             {recCardsList}
+            {/* <Text>{db_recs}</Text> */}
         </ScrollView>
-        {/* <Button title="Login" onPress={() => navigation.navigate("LoginScreen")} /> */}
         <NavBar navigation={navigation} />
     </SafeAreaView>
   );
